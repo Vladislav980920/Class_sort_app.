f63ca862c6d3f02b0eab883d60b91273c2d74a77
Нуар <nmikehin@gmail.com>
2026-02-24 17:09:35 +0400

    Изменил метод в сортировке

--- src/main/java/by/matusevich/sort/app/strategy/FirstNameByAscStrategy.java
+++ src/main/java/by/matusevich/sort/app/strategy/FirstNameByAscStrategy.java
@@ -5,6 +5,6 @@ import by.matusevich.sort.app.model.*;
 public class FirstNameByAscStrategy implements ComparingStrategy {
     @Override
     public int compare(User user1, User user2) {
-        return user1.getEmail().compareTo(user2.getEmail());
+        return user1.getName().compareTo(user2.getName());
     }
 }
